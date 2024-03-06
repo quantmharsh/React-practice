c9d296318ff053d9a6a33979c036c77ab9d25c37
Harsh Srivastava <srivastavah240@gmail.com>
2024-03-07 01:27:43 +0530

    Store setup completed

---
+++ 012MegaBlog/src/store/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice  , nanoid } from "@reduxjs/toolkit";
+
+const initialState ={
+    status: false, 
+    userData:null
+}
+
+export const authSlice=  createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        //Action 1
+        login:(state ,action )=>{
+            state.status=true
+            state.userData=action.payload.userData;
+        },
+        //Action 2 
+        logout:(state)=>{
+            state.status=false;
+            state.userData=null;
+
+        }
+
+    }
+
+})
+export default authSlice.reducer;
